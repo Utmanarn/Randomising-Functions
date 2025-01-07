a866f50ff76e3d6789dc586ba46954de71e86ff2
Utmanarn <Utmanarn@gmail.com>
2025-01-07 18:25:58 +0100

    Update GameLogic.cs
    Added a rolling interaction.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour
 {
+    private DiceHand _playerHand;
+    [SerializeField] private Button _rollButton;
 
-
-    public void Update()
+    private void Awake()
     {
+        _playerHand = GetComponent<DiceHand>();
+    }
 
+    public void OnRoll()
+    {
+        _rollButton.interactable = false;
     }
 }
